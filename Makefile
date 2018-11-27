41d3ed5d9ca517c71155b51b0bf3320e998b7c06
Shintaro Abe <dictav@gmail.com>
2018-11-27 20:07:01 +0900

    update to support gzip

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	docker-compose up
+	docker-compose up --build
 	@echo "PLEASE access to http://localhost:8080/log"
 	@echo "And check /mylog directory"
--- backend/main.go
+++ backend/main.go
@@ -6,22 +6,28 @@ import (
 	"time"
 )
 
-var files = []string{
-	"file_a",
-	"file_b",
-	"file_c",
+var versions = []string{
+	"",
+	"10",
+	"11",
+	"12",
+	"13",
+	"14",
 }
 
 func main() {
 	rand.Seed(time.Now().Unix())
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		idx := rand.Perm(len(files))[0]
-		rw.Header().Set("X-Log-File", files[idx])
+		idx := rand.Perm(len(versions))[0]
+		v := versions[idx]
+		if v != "" {
+			rw.Header().Set("X-Version", v)
+		}
 	})
 
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
 
